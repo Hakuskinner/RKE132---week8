29215c7e85a55a8eb8373dd5ab22d96aa6044c6b
Maiko <maiko.magi@gmail.com>
2022-12-30 14:59:43 +0200

    first commit

---
+++ week8/Program.cs
@@ -0,0 +1,29 @@
+﻿//string[] heroes = { "Harry Potter", "Luke Skywalker", "Lara Croft", "Scooby-Doo" };
+//string[] villians = { "Voldemort", "Darth Vader", "Draculla", "Joker", "Sauron" };
+
+
+string folderPath = @"C:\Users\SpireBlack\Documents\";
+string heroFile = "heroes.txt";
+string villianFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath,heroFile));
+string[] villians = File.ReadAllLines(Path.Combine(folderPath,villianFile));
+
+
+string[] weapon = { "Magic wand", "Plastic fork", "Banana", "Wooden sword", "Lego brick" };
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} with {heroWeapon} saves the day!");
+
+string villian = GetRandomValueFromArray(villians);
+string villianWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villian} with {villianWeapon} tries to take over the world!");
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
